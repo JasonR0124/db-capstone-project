f3791b416955a1a32d0d155f0c5ceec6082fb346
JasonR0124 <jason.reed.0203@gmail.com>
2024-02-27 22:56:13 -0600

    Added Little Lemon DB Info and ERD

---
+++ db-capstone-project/LittleLemonDB.sql
@@ -0,0 +1,192 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: littlelemondb
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bookings`
+--
+
+DROP TABLE IF EXISTS `bookings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bookings` (
+  `Booking_ID` int NOT NULL AUTO_INCREMENT,
+  `Booking_Date` date NOT NULL,
+  `Booking_Slot` time NOT NULL,
+  `Table_Number` int NOT NULL,
+  `Customer_ID` int NOT NULL,
+  PRIMARY KEY (`Booking_ID`),
+  KEY `Customer_ID_idx` (`Customer_ID`),
+  CONSTRAINT `Customer_ID` FOREIGN KEY (`Customer_ID`) REFERENCES `customers` (`Customer_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bookings`
+--
+
+LOCK TABLES `bookings` WRITE;
+/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `customers`
+--
+
+DROP TABLE IF EXISTS `customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `customers` (
+  `Customer_ID` int NOT NULL,
+  `Customer_Name` varchar(255) NOT NULL,
+  `Contact_Number` varchar(45) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  PRIMARY KEY (`Customer_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `menu`
+--
+
+DROP TABLE IF EXISTS `menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `menu` (
+  `Menu_ID` int NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Type` varchar(45) NOT NULL,
+  `Cuisine` varchar(45) NOT NULL,
+  `Price` decimal(2,0) NOT NULL,
+  PRIMARY KEY (`Menu_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `menu`
+--
+
+LOCK TABLES `menu` WRITE;
+/*!40000 ALTER TABLE `menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_delivery_status`
+--
+
+DROP TABLE IF EXISTS `order_delivery_status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_delivery_status` (
+  `Delivery_ID` int NOT NULL,
+  `Delivery_Date` date NOT NULL,
+  `Status` varchar(45) NOT NULL,
+  `Staff_ID` int NOT NULL,
+  PRIMARY KEY (`Delivery_ID`),
+  KEY `Staff_ID_idx` (`Staff_ID`),
+  CONSTRAINT `Staff_ID` FOREIGN KEY (`Staff_ID`) REFERENCES `staff_information` (`Staff_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_delivery_status`
+--
+
+LOCK TABLES `order_delivery_status` WRITE;
+/*!40000 ALTER TABLE `order_delivery_status` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_delivery_status` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `Order_ID` int NOT NULL AUTO_INCREMENT,
+  `Order_Date` date NOT NULL,
+  `Menu_ID` int NOT NULL,
+  `Quantity` int NOT NULL,
+  `Total_Cost` decimal(2,0) NOT NULL,
+  `Delivery_ID` int NOT NULL,
+  `Booking_ID` int NOT NULL,
+  PRIMARY KEY (`Order_ID`),
+  KEY `Menu_ID_idx` (`Menu_ID`),
+  KEY `Delivery_ID_idx` (`Delivery_ID`),
+  KEY `Booking_ID_idx` (`Booking_ID`),
+  CONSTRAINT `Booking_ID` FOREIGN KEY (`Booking_ID`) REFERENCES `bookings` (`Booking_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Delivery_ID` FOREIGN KEY (`Delivery_ID`) REFERENCES `order_delivery_status` (`Delivery_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `Menu_ID` FOREIGN KEY (`Menu_ID`) REFERENCES `menu` (`Menu_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff_information`
+--
+
+DROP TABLE IF EXISTS `staff_information`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `staff_information` (
+  `Staff_ID` int NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Role` varchar(45) NOT NULL,
+  `Salary` int NOT NULL,
+  PRIMARY KEY (`Staff_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff_information`
+--
+
+LOCK TABLES `staff_information` WRITE;
+/*!40000 ALTER TABLE `staff_information` DISABLE KEYS */;
+/*!40000 ALTER TABLE `staff_information` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-27 22:53:33
